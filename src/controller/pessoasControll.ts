b3720745d9e722341bb25fc302e8c687e09d8052
real <58672752+R34ll@users.noreply.github.com>
2024-07-17 22:43:37 -0300

    fix(controller) fix express import and endpoint responses

--- src/controller/pessoasControll.ts
+++ src/controller/pessoasControll.ts
@@ -1,10 +1,26 @@
+import { Response, Request } from "express";
 
 
-export function countPessoas(request:Request, response:Response):void{}
-export function getPessoas(request:Request, response:Response):void{}
-export function getPessoaById(request:Request, response:Response):void{}
-export function removePessoaById(request:Request, response:Response):void{}
-export function createPessoa(request:Request, response:Response):void{}
+export function countPessoas(request:Request, response:Response):void{
+    response.send("todo")
+}
+export function getPessoas(request:Request, response:Response):void{
+    response.send("todo")
+
+}
+export function getPessoaById(request:Request, response:Response):void{
+    response.send("todo")
+
+}
+export function removePessoaById(request:Request, response:Response):void{
+    response.send("todo")
+
+}
+
+export function createPessoa(request:Request, response:Response):void{
+    response.send("todo")
+
+}
 
 
 
