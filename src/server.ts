68ab8e1045ac07348ff0cae421d5f4602332da35
real <58672752+R34ll@users.noreply.github.com>
2024-07-17 22:32:17 -0300

    feat(server): create Express server entry point
    - Implemented basic Express server setup with initial route for test connection
    - Added Pessoas route

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import { Router, Request, Response } from "express";
+import pessoasRouter from "./routes/pessoasRouter";
+
+
+
+const app = express();
+const router = Router();
+const PORT = process.env.EXPRESS_PORT || 3000;
+
+app.use(router)
+app.use(pessoasRouter)
+
+router.get("/ping",(request: Request, response: Response) =>{
+    response.json({
+        message:"pong"
+    }).status(202);
+})
+
+
+app.listen(PORT, () =>
+    console.log(`Servidor aberto na porta ${PORT}`)
+)
