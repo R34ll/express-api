f14f41c39c69f4b02a190d4621225c87812de19d
real <58672752+R34ll@users.noreply.github.com>
2024-07-17 23:27:50 -0300

    feat(model): Created method to record new Pessoas in database

--- src/models/pessoas.ts
+++ src/models/pessoas.ts
@@ -24,38 +24,42 @@ enum Stack {
 // Classe representando a entidade Pessoa
 export class Pessoas {
     private id: number // Unico
-    public readonly apelido:string // Unico
+    public readonly apelido: string // Unico
     public readonly nome: string;
     public readonly nascimento: string;
     public readonly stacks: Set<Stack>;
 
-    constructor(apelido: string, nome: string, nascimento:string, stack: Set<Stack>){ 
-                if(stack.size > Object.keys(Stack).length ){
-                    throw new ApiError(`Invalid size of stack: ${stack.size}`, 500);
-                }
-        
-                for (const item of stack) {
-                    if(!Object.values(Stack).includes(item.toLowerCase() as Stack)){
-                        throw new ApiError(`Valor de 'stack' invalido: ${item}.`, 500);
-                    }
-                }
-        
-                if(apelido.length > 30){ 
-                    throw new ApiError("'apelido' deve ter menos de 30 caracteres. ",500);
-                }
-        
-                if(nome.length > 100){
-                    throw new ApiError("'nome' deve ter menos de 30 caracteres.",500);
-                }
-        
-                if(!(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/.test(nascimento))){
-                    throw new ApiError("Formato da data de nascimento errada. Siga o formato YYYY-MM-DD",500);
-                }
-        
+    constructor(apelido: string, nome: string, nascimento: string, stack: Set<Stack>) {
+        if(!apelido || !nome || !nascimento){
+            throw new ApiError("Preencha todos os campos.",500);
+        }
 
-        
+        if (stack.size > Object.keys(Stack).length) {
+            throw new ApiError(`Tamanho de stacks '${stack.size}' invalido.`, 500);
+        }
+
+        for (const item of stack) {
+            if (!Object.values(Stack).includes(item.toLowerCase() as Stack)) {
+                throw new ApiError(`Valor de '${item}' invalido.`, 500);
+            }
+        }
+
+        if (apelido.length > 30) {
+            throw new ApiError("'apelido' deve ter menos de 30 caracteres.", 500);
+        }
+
+        if (nome.length > 100) {
+            throw new ApiError("'nome' deve ter menos de 30 caracteres.", 500);
+        }
+
+        if (!(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/.test(nascimento))) {
+            throw new ApiError("Formato da data de nascimento errada. Siga o formato YYYY-MM-DD", 500);
+        }
+
+
+
         this.id = this.generateId(); // Gera, por incremento automático(ordem crescente), o ID 
-        this.apelido = apelido; 
+        this.apelido = apelido;
         this.nome = nome;
         this.nascimento = nascimento;
         this.stacks = stack;
@@ -63,31 +67,46 @@ export class Pessoas {
     }
 
     // Garante ID unico e auto incrementado
-    private generateId(): number{
+    private generateId(): number {
         const pessoas = Pessoas.findAll();
-        return pessoas[pessoas.length-1].id + 1;
+        return pessoas[pessoas.length - 1].id + 1;
     }
 
-    public static findAll():Pessoas[]{
+    public static findAll(): Pessoas[] {
         try {
             const data = fs.readFileSync(dataPath, 'utf-8');
             const pessoas: Pessoas[] = JSON.parse(data);
-    
+
             return pessoas;
         } catch (error) {
-            throw new ApiError(`Erro ao tentar ler arquivo local de dados.: ${error}`,500)
+            throw new ApiError(`Erro ao tentar ler arquivo local de dados: ${error}`, 500)
         }
     }
 
-    public static findByid(id: number):Pessoas{
+    public static findByid(id: number): Pessoas {
         const pessoas = Pessoas.findAll();
         const pessoa: Pessoas | undefined = pessoas.find(p => p.id === id);
 
-        if (!pessoa){throw new ApiError(`Pessoa with id '${id}' not found.`, 404);}
+        if (!pessoa) { throw new ApiError(`Pessoa with id '${id}' not found.`, 404); }
 
 
         return pessoa;
 
+    }
+
+    public save(): void{
+        try {
+            const data = fs.readFileSync(dataPath, 'utf-8');
+            const pessoas: Pessoas[] = JSON.parse(data);
+            
+            pessoas.push(this);
+            const newData = JSON.stringify(pessoas, null, 2);
+
+            fs.writeFileSync(dataPath, newData, 'utf-8');
+
+        } catch (error) {
+            throw new ApiError(`Erro ao tentar salvar nova pessoa: ${error}.`, 500);
+        }
     }
 
 }
